664dfb17bae53f6bac7d89f0bc451da289d84664
Martin <martinfz@t-online.de>
2023-05-10 23:57:16 +0200

    INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,30 @@
+# Team Map in Python
+A python script to return a HTML map of Germany.
+Includes Pins in Cities you specified.
+
+
+## Prerequisites
+- `python3-venv`
+- `python3-pip`
+
+# 🚀 USAGE
+- `git clone git@github.com:zwoefler/TeamMembersMapPy.git`
+- `cd TeamMembersMapPy`
+- `python3 -m venv Env`
+- `source Env/bin/activate`
+- `pip install -r requirements.txt`
+- `python3 map.py`
+
+To change the cities change the "locations.txt" file.
+The format is 
+
+```txt
+<NAME>, <GERMAN CITY>
+<NAME>, <GERMAN CITY>
+
+```
+
+## ⚠️  WARNING
+- NO TESTS
+- TYPICAL ONE WAY SCRIPT
+- NOT MOBILE OPTIMIZED
---
+++ map.py
@@ -0,0 +1,29 @@
+import folium
+from geopy.geocoders import Nominatim
+
+# Read the team member locations from the file
+with open('locations.txt', 'r') as file:
+    team_locations = [line.strip() for line in file.readlines()]
+
+# Create the map
+map = folium.Map(location=[51.1657, 10.4515], zoom_start=6)
+
+# Geocode and add markers for each team member's location
+geolocator = Nominatim(user_agent="my_team_map")
+for location in team_locations:
+    try:
+        name, city = location.split(', ')
+        city = city + ", Germany"
+        address = f"{name}, {city}"
+        loc = geolocator.geocode(city)
+        if loc is not None:
+            marker = folium.Marker(location=[loc.latitude, loc.longitude], popup=address)
+            marker.add_to(map)
+        else:
+            print(f"Failed to geocode location: {location}. No result found.")
+    except Exception as e:
+        print(f"Failed to geocode location: {location}. Error: {str(e)}")
+
+# Save the map to an HTML file
+map.save('team_map.html')
+
